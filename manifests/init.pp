709cd23d36484b9c64745a046ccea97fbc95af20
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2012-05-18 11:57:11 +0200

    Add missing init.pp changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,4 +8,22 @@ class puppet {
 		 context => "/files/etc/puppet/puppet.conf",
 		 changes => "set agent/preferred_serialization_format yaml"
 	}
+
+	augeas { "agent_listener":
+		context => "/files/etc/puppet/puppet.conf",
+		changes => "set agent/listen true"
+	}
+
+	$provisioning_host = hiera("provisioning_host")
+
+	file { '/etc/puppet/namespaceauth.conf':
+		ensure => present,
+		content => template('puppet/namespaceauth.conf.erb')
+	}
+
+	file { '/etc/puppet/auth.conf':
+		ensure => present,
+		content => template('puppet/auth.conf.erb')
+	}
+
 }
