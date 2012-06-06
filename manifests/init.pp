33f39ca6419f29c437b7337a11028db0145ddcf0
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2012-06-06 12:58:10 +0200

    Update to new module style

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,27 +1,28 @@
-class puppet {
-  $provisioning_host = hiera('provisioning_host')
+class puppet (
+    $provisioning_host = params_loookup('provisioning_host')
+    ) {
 
-  # pson output breaks sometimes with hiera
-  # see http://projects.puppetlabs.com/issues/13212
-  # therefore we enforce yaml here. maybe slower, but works
-  augeas { 'puppet.conf':
-    context => '/files/etc/puppet/puppet.conf',
-    changes => 'set agent/preferred_serialization_format yaml',
-  }
+    # pson output breaks sometimes with hiera
+    # see http://projects.puppetlabs.com/issues/13212
+    # therefore we enforce yaml here. maybe slower, but works
+    augeas { 'puppet.conf':
+        context => '/files/etc/puppet/puppet.conf',
+        changes => 'set agent/preferred_serialization_format yaml',
+    }
 
-  augeas { 'agent_listener':
-    context => '/files/etc/puppet/puppet.conf',
-    changes => 'set agent/listen true',
-  }
+    augeas { 'agent_listener':
+        context => '/files/etc/puppet/puppet.conf',
+        changes => 'set agent/listen true',
+    }
 
 
-  file { '/etc/puppet/auth.conf':
-    ensure  => present,
-    content => template('puppet/auth.conf.erb'),
-  }
+    file { '/etc/puppet/auth.conf':
+        ensure  => present,
+        content => template('puppet/auth.conf.erb'),
+    }
 
-  file { '/etc/puppet/namespaceauth.conf':
-    ensure  => present,
-    content => template('puppet/namespaceauth.conf.erb'),
-  }
+    file { '/etc/puppet/namespaceauth.conf':
+        ensure  => present,
+        content => template('puppet/namespaceauth.conf.erb'),
+    }
 }
